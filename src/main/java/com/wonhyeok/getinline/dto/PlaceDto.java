0ea7ecd091f532b79c75ee547904ad88f3f69aa9
wonhyoek <epahsejtmxm@naver.com>
2022-01-17 17:08:22 +0900

    Implement - dtos and contants

---
+++ src/main/java/com/wonhyeok/getinline/dto/PlaceDto.java
@@ -0,0 +1,69 @@
+package com.wonhyeok.getinline.dto;
+
+import com.wonhyeok.getinline.constant.PlaceType;
+import com.wonhyeok.getinline.domain.Place;
+import com.wonhyeok.getinline.domain.Place;
+
+import java.time.LocalDateTime;
+
+public record PlaceDto(
+        Long id,
+        PlaceType placeType,
+        String placeName,
+        String address,
+        String phoneNumber,
+        Integer capacity,
+        String memo,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
+) {
+
+    public static PlaceDto idOnly(Long id) {
+        return PlaceDto.of(id, null, null, null, null, null, null, null, null);
+    }
+
+    public static PlaceDto of(
+            Long id,
+            PlaceType placeType,
+            String placeName,
+            String address,
+            String phoneNumber,
+            Integer capacity,
+            String memo,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt
+    ) {
+        return new PlaceDto(id, placeType, placeName, address, phoneNumber, capacity, memo, createdAt, modifiedAt);
+    }
+
+    public static PlaceDto of(Place place) {
+        return new PlaceDto(
+                place.getId(),
+                place.getPlaceType(),
+                place.getPlaceName(),
+                place.getAddress(),
+                place.getPhoneNumber(),
+                place.getCapacity(),
+                place.getMemo(),
+                place.getCreatedAt(),
+                place.getModifiedAt()
+        );
+    }
+
+
+    public Place toEntity() {
+        return Place.of(placeType, placeName, address, phoneNumber, capacity, memo);
+    }
+
+    public Place updateEntity(Place place) {
+        if (placeType != null) { place.setPlaceType(placeType); }
+        if (placeName != null) { place.setPlaceName(placeName); }
+        if (address != null) { place.setAddress(address); }
+        if (phoneNumber != null) { place.setPhoneNumber(phoneNumber); }
+        if (capacity != null) { place.setCapacity(capacity); }
+        if (memo != null) { place.setMemo(memo); }
+
+        return place;
+    }
+
+}
